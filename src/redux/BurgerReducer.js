970d04946097e52c3034376d8bc53303db36f5e2
trangchongcheng <cheng.cit@gmail.com>
2020-05-24 10:21:47 +0700

    Fix bug add item

--- src/redux/BurgerReducer.js
+++ src/redux/BurgerReducer.js
@@ -1,33 +1,39 @@
 const burgerState = {
   burger: { salad: 1, cheese: 1, beef: 1 },
-  menu: { salad: 10, cheese: 10, beef: 10 },
-  total: 30,
+  menu: { salad: 10, cheese: 20, beef: 55 },
+  total: 85,
 };
 
 const BurgerReducer = (state = burgerState, action) => {
   switch (action.type) {
     case "ADD_ITEM": {
       // Add Food Items
-      let burgers = state.burger
+      let burgers = state.burger;
+      let menu = state.menu;
       for (const i in burgers) {
         if (action.key === i) {
-            burgers[i] += 1;
-            state.total += 10;
+          burgers[i] += 1;
+          state.total += menu[i];
         }
       }
-      state.burger = Object.assign(burgers);
-      return {...state};
- 
+      state.burger = { ...burgers };
+      return { ...state };
     }
 
     case "REMOVE_ITEM": {
-        for (const i in state.burger) {
-            if (action.key === i) {
-                state.burger[i] += 1;
-                state.total -= 10;
-            }
+      let burgers = state.burger;
+      let menu = state.menu;
+      for (const i in burgers) {
+        if (action.key === i) {
+          if(burgers[i]>0){
+            burgers[i] -= 1;
+            state.total -= menu[i];
           }
-          return { ...state };
+        
+        }
+      }
+      state.burger = { ...burgers };
+      return { ...state };
     }
   }
 
